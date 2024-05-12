46b11e46711f44feb43d79a09830912a878f68d9
lucas <lucas.cavalcante0013@gmail.com>
2024-05-12 17:43:28 -0300

    Tela inical editext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,27 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btcalcular = findViewById<Button>(R.id.btcalcular)
+
+        btcalcular.setOnClickListener {
+            val peso = edtPeso.text
+            println("O peso é " + peso)
+
+            val altura = edtAltura.text
+            println(" sua altura é" + altura)
+
+        }
     }
 }
